73906bdd0dbc7422d63b739bd5877f2b4b0bd1db
Lucas Oliveira <lucas.fullstack.dev@gmail.com>
2026-05-01 12:44:12 -0300

    refactor: removendo 'preventOverHead' do BaseTheme

--- themes/BaseTheme/Theme.php
+++ themes/BaseTheme/Theme.php
@@ -28,27 +28,4 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme
 
         parent::__construct($config);
     }
-
-    function _init()
-    {
-        parent::_init();
-        $app = App::i();
-
-        /**
-         * Implementação que impede a sobrecarga de dados do usuário administrativo
-         * Pull Request https://github.com/culturagovbr/mapadacultura/pull/53
-         * TODO: Necessário implementar interface para gestão desse metadado
-         */
-        $app->hook('auth.successful', function () use ($app) {
-            $user = $app->user;
-
-            $preventOverhead = (bool) ($user->metadata['preventOverhead'] ?? false);
-            if (!$preventOverhead) {
-                $user->getEntitiesNotifications($app);
-            }
-
-            $user->lastLoginTimestamp = new \DateTime;
-            $user->save(true);
-        });
-    }
 }
